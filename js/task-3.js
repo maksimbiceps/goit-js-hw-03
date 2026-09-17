fd3011f4435bb4ac3d36f6b8905d5bc0b2f229a4
Maksim <Maksimka.Kravecz@gmail.com>
2026-09-17 20:45:10 +0300

    homework

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,20 @@
 'use strict';
 
+function filterArray(numbers, value) {
+    const filtered = [];
 
-function checkForSpam(message) {
+    for (let i = 0; i < numbers.length; i += 1){
+        if (numbers[i] > value) {
+            filtered.push(numbers[i]);
+        }
+    }
+    return filtered;
 
-    const lowerMessage = message.toLowerCase(); 
-    return lowerMessage.includes('spam') || lowerMessage.includes('sale');
+}
 
- }
-    
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
